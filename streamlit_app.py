27c180421c63ecec71403beb0b6c8b2764bc0e9a
Luan Fernandes <luan.fernandes@buser.com.br>
2023-10-15 23:06:02 -0300

    first commit

---
+++ streamlit_app.py
@@ -0,0 +1,7 @@
+"""Python file to serve as the frontend"""
+import streamlit as st
+import chromadb # check if chromadb was successfully imported
+
+# From here down is all the StreamLit UI.
+st.set_page_config(page_title="LangChain Demo", page_icon=":robot:")
+st.header("Maria dos Anjos Demo")
