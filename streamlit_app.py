1f21ae2238eccdbaeb3ab561005a567144251b80
Luan Fernandes <luan.fernandes@buser.com.br>
2023-10-15 23:27:29 -0300

    overwrite deafault sqlite3

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,13 @@
 import streamlit as st
 
 # check if chromadb was successfully imported
+# following this troubleshooting https://docs.trychroma.com/troubleshooting#sqlite
 import chromadb
+# these three lines swap the stdlib sqlite3 lib with the pysqlite3 package
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 from langchain.vectorstores import Chroma
 from langchain.embeddings.openai import OpenAIEmbeddings
 
