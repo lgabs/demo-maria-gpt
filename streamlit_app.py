ddde4910d1047f6fd8f1e2b0b9889e246fac3ca3
Luan Fernandes <luan.fernandes@buser.com.br>
2023-10-15 23:09:59 -0300

    some more imports to show usage

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,15 @@
 """Python file to serve as the frontend"""
 import streamlit as st
-import chromadb # check if chromadb was successfully imported
+
+# check if chromadb was successfully imported
+import chromadb
+from langchain.vectorstores import Chroma
+from langchain.embeddings.openai import OpenAIEmbeddings
+
+# how I am using chroma as a vectorstore
+# docsearch = Chroma(
+#     persist_directory="embeddings", embedding_function=OpenAIEmbeddings()
+# )
 
 # From here down is all the StreamLit UI.
 st.set_page_config(page_title="LangChain Demo", page_icon=":robot:")
